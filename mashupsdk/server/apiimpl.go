f3457b3eee25159a46ab8187c2fae8f9701bb50d
Joel Rieke <>
2022-04-11 06:13:39 -0700

    Adding very basic collaboration api stubs to implement later.

--- mashupsdk/server/apiimpl.go
+++ mashupsdk/server/apiimpl.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"time"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	sdk "tini.com/nute/mashupsdk"
 )
 
@@ -52,6 +54,26 @@ func (s *MashupServer) OnResize(ctx context.Context, in *sdk.MashupDisplayBundle
 		log.Printf("Delegate to api handler.")
 		s.mashupApiHandler.OnResize(displayHint)
 	}
+
+	return nil, nil
+}
+
+func (mhs *MashupServer) UpsertMashupSociety(ctx context.Context, in *sdk.MashupSocietyBundle) (*sdk.MashupSocietyStateBundle, error) {
+	log.Printf("UpsertMashupSociety called")
+	if in.GetAuthToken() != serverConnectionConfigs.AuthToken {
+		return nil, errors.New("Auth failure")
+	}
+	// TODO: Implement.
+
+	return nil, status.Errorf(codes.Unimplemented, "method UpsertMashupSociety not implemented")
+}
+
+func (mhs *MashupServer) UpsertMashupSocietyState(ctx context.Context, in *sdk.MashupSocietyStateBundle) (*sdk.MashupSocietyStateBundle, error) {
+	log.Printf("UpsertMashupSocietyState called")
+	if in.GetAuthToken() != serverConnectionConfigs.AuthToken {
+		return nil, errors.New("Auth failure")
+	}
+	// TODO: Implement.
 
 	return nil, nil
 }
