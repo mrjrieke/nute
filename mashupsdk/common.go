a14b006839df8f1b1044f0ff4458d0ab5e2b0f25
Joel Rieke <>
2022-05-23 05:00:27 -0700

    Start of a little logic combo.

--- mashupsdk/common.go
+++ mashupsdk/common.go
@@ -7,9 +7,75 @@ const (
 	Moved   int64 = 3
 )
 
+type MashupDisplayContext struct {
+	MainWinDisplay *MashupDisplayHint
+	yOffset        int
+	settled        int
+}
+
+func (m *MashupDisplayContext) GetYoffset() int {
+	return m.yOffset
+}
+
+func (m *MashupDisplayContext) SetYoffset(a int) int {
+	m.yOffset = a
+	return m.yOffset
+}
+
+func (m *MashupDisplayContext) GetSettled() int {
+	return m.settled
+}
+
+func (m *MashupDisplayContext) ApplySettled(s int, override bool) int {
+	if override {
+		m.settled = s
+	} else {
+		m.settled |= s
+	}
+	return m.settled
+}
+
+func (m *MashupDisplayContext) OnResize(displayHint *MashupDisplayHint) bool {
+	resize := false
+	if m.MainWinDisplay == nil {
+		resize = true
+		m.MainWinDisplay = &MashupDisplayHint{}
+	}
+
+	if displayHint == nil {
+		return false
+	}
+
+	if displayHint.Xpos != 0 && (*m.MainWinDisplay).Xpos != displayHint.Xpos {
+		m.MainWinDisplay.Xpos = displayHint.Xpos
+		resize = true
+	}
+	if displayHint.Ypos != 0 && (*m.MainWinDisplay).Ypos != displayHint.Ypos {
+		m.MainWinDisplay.Ypos = displayHint.Ypos + int64(m.yOffset)
+		resize = true
+	}
+	if displayHint.Width != 0 && (*m.MainWinDisplay).Width != displayHint.Width {
+		m.MainWinDisplay.Width = displayHint.Width
+		resize = true
+	}
+	if displayHint.Height != 0 && (*m.MainWinDisplay).Height != displayHint.Height+int64(m.yOffset) {
+		m.MainWinDisplay.Height = displayHint.Height
+		resize = true
+	}
+
+	if m.settled < 15 {
+		return false
+	} else if m.settled == 15 {
+		resize = true
+		m.settled = 31
+	}
+	return resize
+}
+
 // MashupApiHandler -- mashups implement this to handle all events sent from
 // other mashups.
 type MashupApiHandler interface {
+	//	InitDisplayContext(mashupDisplayContext *MashupDisplayContext)
 	OnResize(displayHint *MashupDisplayHint)
 	UpsertMashupElements(detailedElementBundle *MashupDetailedElementBundle) (*MashupElementStateBundle, error)
 	UpsertMashupElementsState(elementStateBundle *MashupElementStateBundle) (*MashupElementStateBundle, error)
