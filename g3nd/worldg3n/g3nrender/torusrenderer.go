aa3f506ae2c71bdf9e8dd91f61c87080c1749dd1
Joel Rieke <mrjrieke@gmail.com>
2022-08-03 05:20:20 -0700

    Partial multi select.

--- g3nd/worldg3n/g3nrender/torusrenderer.go
+++ g3nd/worldg3n/g3nrender/torusrenderer.go
@@ -65,11 +65,28 @@ func (tr *TorusRenderer) Layout(worldApp *g3nworld.WorldApp,
 	tr.GenericRenderer.LayoutBase(worldApp, tr, g3nRenderableElements)
 }
 
+func (tr *TorusRenderer) TorusParser(worldApp *g3nworld.WorldApp, childId int64) {
+	child := worldApp.ConcreteElements[childId]
+	if !child.IsAbstract() {
+		log.Printf("Child Item removed %s: %v", child.GetDisplayName(), worldApp.RemoveFromScene(child.GetNamedMesh(child.GetDisplayName())))
+	}
+
+	if len(child.GetChildElementIds()) > 0 {
+		for _, cId := range child.GetChildElementIds() {
+			tr.TorusParser(worldApp, cId)
+		}
+	}
+}
+
 func (tr *TorusRenderer) HandleStateChange(worldApp *g3nworld.WorldApp, g3nDetailedElement *g3nmash.G3nDetailedElement) bool {
 	var g3nColor *math32.Color
 
 	if g3nDetailedElement.IsStateSet(mashupsdk.Hidden) {
-		log.Printf("Item removed %s: %v", g3nDetailedElement.GetDisplayName(), worldApp.RemoveFromScene(g3nDetailedElement.GetNamedMesh(g3nDetailedElement.GetDisplayName())))
+		if g3nDetailedElement.GetDetailedElement().Genre == "Collection" && g3nDetailedElement.GetDetailedElement().Subgenre == "Torus" {
+			tr.TorusParser(worldApp, g3nDetailedElement.GetDetailedElement().Id)
+		} else {
+			log.Printf("Item removed %s: %v", g3nDetailedElement.GetDisplayName(), worldApp.RemoveFromScene(g3nDetailedElement.GetNamedMesh(g3nDetailedElement.GetDisplayName())))
+		}
 		return true
 	} else {
 		worldApp.UpsertToScene(g3nDetailedElement.GetNamedMesh(g3nDetailedElement.GetDisplayName()))
