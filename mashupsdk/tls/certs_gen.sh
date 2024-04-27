5e82feb9a65054f1aa42801b1a60239c8e73cd7c
Joel Rieke <joel@skykomish.tini.com>
2024-04-27 10:15:26 -0700

    2-step to make more valid self signed certs.

--- mashupsdk/tls/certs_gen.sh
+++ mashupsdk/tls/certs_gen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout mashup.key -out mashup.crt
-openssl req -new -x509 -nodes -days 365 -config san.cnf -keyout mashup.key -out mashup.crt
+openssl req -new -nodes -newkey rsa:2048 -config san.cnf -reqexts v3_req -keyout mashup.key -out mashup.csr
+
+openssl x509 -req -in mashup.csr -extfile san.cnf -extensions v3_req -signkey mashup.key -days 365 -out mashup.crt
 
 mkdir -p examples/helloworld/hellofyne/tls
 cp mashup.key examples/helloworld/hellofyne/tls
